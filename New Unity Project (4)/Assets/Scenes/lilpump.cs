e267f38801089e906c815f6f9b9e62ea4d0c5b24
jtorso03 <38812974+jtorso03@users.noreply.github.com>
2018-04-28 16:41:52 +0200

    EL CUBO QUE ROTA POR EL COMUNISMO

---
+++ New Unity Project (4)/Assets/Scenes/lilpump.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lilpump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Rotate(70 * Time.deltaTime, 70 * Time.deltaTime, 70 * Time.deltaTime) ;
+
+        
+	}
+}
